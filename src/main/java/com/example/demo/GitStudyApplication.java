5cd8376ca4c0e58f160c0d36151adba058d75631
hyeonmin <poppy3672@naver.com>
2025-08-19 18:32:13 +0900

    first commit

--- src/main/java/com/example/demo/GitStudyApplication.java
+++ src/main/java/com/example/demo/GitStudyApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitStudyApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("new line");
 		SpringApplication.run(GitStudyApplication.class, args);
 	}
+
 
 }
 
